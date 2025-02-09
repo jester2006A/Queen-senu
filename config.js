bba5bb87bc6b74163fab23f33986c3c6324e782a
jester2006A <cyberjester39@gmail.com>
2025-02-10 03:02:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "KAVI-EXE=4BA1SabY#WCF6LTi2MJmNg78h-YzyYgL0GF0NZPZ0SmZfznyFYXY",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/4wFM1rbZ/temp-image.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "🤖𝗛𝗘𝗬 𝗜 AM Queen senu 𝗪𝗛𝗔𝗧𝗦𝗔𝗣𝗣 𝗕𝗢𝗧 🤖𝙳𝙴𝙿𝙻𝚈 𝙱𝚈 jester  🧑‍💻",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
